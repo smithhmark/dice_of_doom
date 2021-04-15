0326fdfae836867f8153780bfe67c34cbec0e9f0
Mark Smith <smith.h.mark@gmail.com>
2021-04-14 22:45:54 -0400

    able to print the board

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,25 @@ fn new_board(size: usize) -> Vec<Hex> {
     hs
 }
 
+fn print_board(hs: Vec<Hex>, sz: usize) {
+    for row in 0..sz {
+        let off_set = sz - row;
+        for _row_skew in 0..off_set {
+            print!("   ");
+        }
+        for col in 0..sz {
+            let idx = sz * row + col;
+            print!("{} ", format!("{}-{}", hs[idx].owner, hs[idx].dice));
+        }
+        print!("\n");
+    }
+}
+
 fn main() {
     println!("Welcome to Dice of Doom, the Rust edition!");
+    let sz = 2;
+    let board = new_board(sz);
+    print_board(board, sz);
 }
 
 #[cfg(test)]

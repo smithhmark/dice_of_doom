7711af296f9d5e0c49d82450c1378db613bab386
Mark Smith <smith.h.mark@gmail.com>
2021-04-14 23:31:47 -0400

    extracting board mod

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,12 @@
-#[derive(Debug)]
-struct Hex {
-    owner: usize,
-    dice: usize,
-}
-
-fn hex_as_string(h: &Hex) -> String {
-    format!("{}-{}", h.owner, h.dice)
-}
-
-fn new_board(size: usize) -> Vec<Hex> {
-    let hex_count = size * size;
-    let mut hs = Vec::with_capacity(hex_count);
-    for _i in 0..hex_count {
-        hs.push(Hex { owner: 0, dice: 0 });
-    }
-    hs
-}
-
-fn board_as_string(hs: &Vec<Hex>, sz: usize) -> String {
-    let mut rep = String::new();
-
-    for row in 0..sz {
-        let off_set = sz - row;
-        for _row_skew in 0..off_set {
-            rep.push_str("   ");
-        }
-        for col in 0..(sz - 1) {
-            let idx = sz * row + col;
-            rep.push_str(&format!("{} ", hex_as_string(&hs[idx])));
-        }
-        let idx = sz * row + sz - 1;
-        rep.push_str(&format!("{}\n", hex_as_string(&hs[idx])));
-    }
-    rep
-}
-
-fn print_board(hs: &Vec<Hex>, sz: usize) {
-    print!("{}", board_as_string(hs, sz));
-}
+mod board;
 
 fn main() {
     println!("Welcome to Dice of Doom, the Rust edition!");
     let sz = 2;
-    let board = new_board(sz);
+    let board = board::new_board(sz);
     print_board(&board, sz);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_build_empty_board() {
-        let sz: usize = 2;
-        let board = new_board(sz);
-        assert_eq!(board.capacity(), board.len());
-        assert_eq!(sz * sz, board.len());
-        for h in &board {
-            assert_eq!(h.owner, 0);
-            assert_eq!(h.dice, 0);
-        }
-    }
-
-    #[test]
-    fn test_board_as_string() {
-        let sz: usize = 2;
-        let board = new_board(sz);
-        let rep = board_as_string(&board, sz);
-        let exp = "      0-0 0-0\n   0-0 0-0\n".to_string();
-        assert_eq!(rep, exp);
-    }
+fn print_board(hs: &board::Board, sz: usize) {
+    print!("{}", board::board_as_string(hs, sz));
 }
